5d4b0d2a3bea494f27a57cb9e0bb00fe9703bc8a
Antonina Muravska <at.tonya@gmail.com>
2020-11-14 01:30:16 +0200

    add task-2

---
+++ js/task-2.js
@@ -0,0 +1,21 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const ingredientsRef = document.getElementById('ingredients');
+console.log(ingredientsRef);
+
+// const itemOfIngridients = document.createElement('li');
+// itemOfIngridients.textContent = 'Картошка';
+// ingredientsRef.appendChild(itemOfIngridients);
+
+ingredients.forEach((item) => {
+    const itemOfIngridients = document.createElement('li');
+    itemOfIngridients.textContent = item;
+    ingredientsRef.appendChild(itemOfIngridients);
+})
