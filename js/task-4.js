d81c46ed71166ffa15a6e98fcbb387312ad4eb7e
Antonina Muravska <at.tonya@gmail.com>
2020-11-14 01:30:33 +0200

    add task-4

---
+++ js/task-4.js
@@ -0,0 +1,19 @@
+const decBtnRef = document.querySelector('[data-action="decrement"]');
+const counterRef = document.getElementById('value');
+const incBtnRef = document.querySelector('[data-action="increment"]');
+
+let counterValue = 0;
+
+const decrement = () => {
+    counterValue -= 1;
+    counterRef.textContent = counterValue;
+
+};
+
+const increment = () => {
+    counterValue += 1;
+    counterRef.textContent = counterValue;
+};
+
+decBtnRef.addEventListener('click', () => { decrement(counterRef) });
+incBtnRef.addEventListener('click', () => { increment(counterRef) });
