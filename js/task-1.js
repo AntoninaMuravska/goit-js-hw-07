4635750bb678c19053a845dc41036e5449923d09
Antonina Muravska <at.tonya@gmail.com>
2020-11-17 11:34:39 +0200

    update task-1

--- js/task-1.js
+++ js/task-1.js
@@ -3,11 +3,19 @@ const categoriesRef = document.querySelectorAll('.item');
 const amountOfCategories = categoriesRef.length;
 console.log(`В списке ${amountOfCategories} категории.`)
 
-const elementsOfCategories = [...categoriesRef];
+// const elementsOfCategories = [...categoriesRef];
 // console.log(elementsOfCategories);
 
-for (let elem of elementsOfCategories) {
-    const titleText = elem.firstElementChild.textContent;
-    const quantityOfElements = elem.lastElementChild.children.length;
+// for (let elem of elementsOfCategories) {
+//     const titleText = elem.firstElementChild.textContent;
+//     const quantityOfElements = elem.lastElementChild.children.length;
+//     console.log(`Категория: ${titleText}\nКоличество элементов: ${quantityOfElements}`)
+// }
+
+// С методом forEach
+
+categoriesRef.forEach(element => {
+    const titleText = element.firstElementChild.textContent;
+    const quantityOfElements = element.lastElementChild.children.length;
     console.log(`Категория: ${titleText}\nКоличество элементов: ${quantityOfElements}`)
-}
+})
