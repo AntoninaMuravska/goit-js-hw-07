adfd4d8039ff8415a3bf5a04249400cf4348d457
Antonina Muravska <at.tonya@gmail.com>
2020-11-17 11:48:50 +0200

    update task-2

--- js/task-2.js
+++ js/task-2.js
@@ -14,8 +14,21 @@ const ingredientsRef = document.getElementById('ingredients');
 // itemOfIngridients.textContent = 'Картошка';
 // ingredientsRef.appendChild(itemOfIngridients);
 
-ingredients.forEach((item) => {
-    const itemOfIngridients = document.createElement('li');
-    itemOfIngridients.textContent = item;
-    ingredientsRef.appendChild(itemOfIngridients);
-})
+// ingredients.forEach((item) => {
+//     const itemOfIngridients = document.createElement('li');
+//     itemOfIngridients.textContent = item;
+//     ingredientsRef.appendChild(itemOfIngridients);
+// })
+
+// создаем массив со всеми li
+const createIngredientsList = (ingredient) => {
+  const itemOfIngridients = document.createElement('li');
+  itemOfIngridients.textContent = ingredient;
+  
+  return itemOfIngridients;
+};
+
+// добавляем их в ДОМ
+const ingredientsList = ingredients.map(element => createIngredientsList(element));
+// console.log(ingredientsList)
+ingredientsRef.append(...ingredientsList)
