8fec57d92dca8f7123207af089ed5b4742ade81b
Antonina Muravska <at.tonya@gmail.com>
2021-06-08 13:32:19 +0300

    fix task-06

--- js/task-06.js
+++ js/task-06.js
@@ -1,16 +1,19 @@
 const inputRef = document.getElementById('validation-input');
 
-const onInputValidation = (event) => {
-    const inputValueLength = event.target.value.length;
-    const dataLength = parseInt(inputRef.dataset.length);
+const onInputValidation = event => {
+  const inputValueLength = event.target.value.length;
+  const dataLength = parseInt(inputRef.dataset.length);
 
-    if (inputValueLength === dataLength) {
-        inputRef.classList.add('valid');
-        inputRef.classList.remove('invalid');
-    } else {
-        inputRef.classList.remove('valid');
-        inputRef.classList.add('invalid');
-    }
+  if (inputValueLength === dataLength) {
+    handleClass('valid', 'invalid');
+  } else {
+    handleClass('invalid', 'valid');
+  }
 };
+
+function handleClass(firstClass, secondClass) {
+  inputRef.classList.add(firstClass);
+  inputRef.classList.remove(secondClass);
+}
 
 inputRef.addEventListener('blur', onInputValidation);
